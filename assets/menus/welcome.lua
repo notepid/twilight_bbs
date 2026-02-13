32a796b0f2b2c17e2f417445863d2a49f7e06fae
Mikael Madsen <m.madsen@sportradar.com>
2026-02-13 16:16:12 +0100

    Fix welcome menu looping

--- assets/menus/welcome.lua
+++ assets/menus/welcome.lua
@@ -2,9 +2,12 @@
 local menu = {}
 
 function menu.on_enter(node)
-    -- Auto-advance to login after displaying
-    node:pause()
+    -- Auto-advance to login after displaying.
+    -- Set navigation first so even if pause is unavailable/errors, we don't loop.
     node:goto_menu("login")
+    if node.pause then
+        node:pause()
+    end
 end
 
 return menu
