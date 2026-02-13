dc443e780e69504214e0a19f20d9528e275a0fc0
Notepid <1101091+notepid@users.noreply.github.com>
2026-02-13 16:57:08 +0100

    Fixed chat hanging on /quit now directly loads main menu

--- assets/menus/welcome.lua
+++ assets/menus/welcome.lua
@@ -2,12 +2,7 @@
 local menu = {}
 
 function menu.on_enter(node)
-    -- Auto-advance to login after displaying.
-    -- Set navigation first so even if pause is unavailable/errors, we don't loop.
     node:goto_menu("login")
-    if node.pause then
-        node:pause()
-    end
 end
 
 return menu
